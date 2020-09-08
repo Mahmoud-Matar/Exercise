5ba5fe7353792655d98cbb19902d248cb68ac91b
Mokhtar2121 <mokhtarsalem@aucegypt.edu>
2020-09-08 14:11:30 +0200

    “message”

--- KnapSack.cpp
+++ KnapSack.cpp
@@ -1,8 +1,8 @@
-#include <KnapSack.h>
+#include "KnapSack.h"
 int KnapSack (int W,int wt[],int val[],int n)
 { if (n==0||W==0)
-return0;
-if(wt[n-1]>W)
+return 0;
+if (wt[n-1]>W)
 return KnapSack (W,wt,val,n-1);
-else 
-return max(val[n-1]+KnapSack(W-wt[n-1],wt,val,n-1),KnapSack(W,Wt,val,n-t));
+else
+return max(val[n-1]+KnapSack(W-wt[n-1],wt,val,n-1),KnapSack(W,Wt,val,n-t));}
