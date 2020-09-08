77254892972dfb4a8bc7da1bf301b9217301db62
Mahmoud_Matar <mahmoud_gamal2001@aucegypt.edu>
2020-09-08 12:21:18 +0300

    Main done

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#inculde <iostream>
+#include <max.h>
+#include <KnapSack.h>
+using namespace std;
+int main ()
+{
+    int val[] = { 60, 100, 120 };
+    int wt[] = { 10, 20, 30 };
+    int W = 50;
+    int n = sizeof(val) / sizeof(val[0]);
+    cout << knapSack(W, wt, val, n);
+
+    return 0;
+
+}
+
